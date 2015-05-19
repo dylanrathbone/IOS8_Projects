f489a8ea939194a04f9aaf74b86d0a3b36764744
Dylan <dylanrathbone@gmail.come>
2015-05-19 21:51:09 +0100

    added CatYears app

--- CatYears/CatYears/ViewController.swift
+++ CatYears/CatYears/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculateAgeTextArea: UITextField!
+    @IBOutlet weak var calculateAgeAnswer: UILabel!
+    
+    @IBAction func calculateAgeButton(sender: AnyObject) {
+        var enteredAge = calculateAgeTextArea.text.toInt()
+        
+        if enteredAge != nil {
+            var catYears = enteredAge! * 7
+            calculateAgeAnswer.text = "Your cat is \(catYears) in cat years"
+        } else {
+            calculateAgeAnswer.text = "Please enter a number"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
